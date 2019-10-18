49ad19bebe69fb469cac9472e45d32a7f6dfd73b
Student <sjoerd0707@live.nl>
2019-10-18 14:06:28 +0200

    Add @OneToMany notation

--- src/main/java/YERgen2/demo/model/Admin.java
+++ src/main/java/YERgen2/demo/model/Admin.java
@@ -2,12 +2,14 @@ package YERgen2.demo.model;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import java.util.List;
 
 @Entity
 @DiscriminatorValue("1")
 public class Admin extends Account {
 
+    @OneToMany
     private List<Tournament> tournament;
 
     public List<Tournament> getTournament() {
