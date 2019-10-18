3168b629551b2b27025397a0e7ab7921e641ae90
Student <sjoerd0707@live.nl>
2019-10-18 15:25:18 +0200

    Basic version of Match.java

--- src/main/java/YERgen2/demo/model/Match.java
+++ src/main/java/YERgen2/demo/model/Match.java
@@ -1,9 +1,7 @@
 package YERgen2.demo.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 public class Match {
@@ -12,4 +10,56 @@ public class Match {
     @GeneratedValue(strategy = GenerationType.AUTO)
     long id;
 
+    @OneToMany
+    private List<Team> teams;
+    private String stage;
+    private String result;
+    private String discipline;
+    private String startTime;
+    private String endTime;
+    private String location;
+
+    public String getLocation() {
+        return location;
+    }
+    public String getEndTime() {
+        return endTime;
+    }
+    public String getStartTime() {
+        return startTime;
+    }
+    public String getDiscipline() {
+        return discipline;
+    }
+    public String getResult() {
+        return result;
+    }
+    public String getStage() {
+        return stage;
+    }
+    public List<Team> getTeams() {
+        return teams;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+    public void setDiscipline(String discipline) {
+        this.discipline = discipline;
+    }
+    public void setResult(String result) {
+        this.result = result;
+    }
+    public void setStage(String stage) {
+        this.stage = stage;
+    }
+    public void setTeams(List<Team> teams) {
+        this.teams = teams;
+    }
 }
