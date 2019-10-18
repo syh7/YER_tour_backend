92bcc5aba131aedf765a00c69031ce62f67ab06b
Student <sjoerd0707@live.nl>
2019-10-18 12:52:51 +0200

    Use correct, non-deprecated library

--- src/main/java/YERgen2/demo/model/User.java
+++ src/main/java/YERgen2/demo/model/User.java
@@ -6,7 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class User {
+public class Users {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     long id;
