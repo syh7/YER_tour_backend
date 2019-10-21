92ce680f02c7f47a2b07bcb34b405dfd5d22da47
Student <sjoerd0707@live.nl>
2019-10-21 10:57:03 +0200

    Remove unused User class

--- src/main/java/YERgen2/demo/controller/UserRespository.java
+++
@@ -1,9 +0,0 @@
-package YERgen2.demo.controller;
-
-import YERgen2.demo.model.User;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Component;
-
-@Component
-public interface UserRespository extends CrudRepository<User, Long> {
-}
