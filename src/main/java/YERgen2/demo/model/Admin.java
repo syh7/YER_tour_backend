bb0ea5da8b102c99f0d00a6e54c758760d18ce2f
Student <sjoerd0707@live.nl>
2019-10-21 10:04:34 +0200

    Add name to Admin accounts

--- src/main/java/YERgen2/demo/model/Admin.java
+++ src/main/java/YERgen2/demo/model/Admin.java
@@ -9,11 +9,19 @@ import java.util.Set;
 @DiscriminatorValue("1")
 public class Admin extends Account {
 
+    private String name;
     @OneToMany
     private Set<Tournament> tournament;
 
+    public String getName() {
+        return name;
+    }
     public Set<Tournament> getTournament() {
         return tournament;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
     public void setTournament(Set<Tournament> tournament) {
         this.tournament = tournament;
