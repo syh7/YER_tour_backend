1f86afa568a759855f4e7dafeb0eb3d1f69fab1d
Student <sjoerd0707@live.nl>
2019-10-21 09:38:43 +0200

    Rename functions and use Set instead of List

--- src/main/java/YERgen2/demo/api/AdminEndpoint.java
+++ src/main/java/YERgen2/demo/api/AdminEndpoint.java
@@ -6,7 +6,7 @@ import YERgen2.demo.model.Admin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping
@@ -16,28 +16,29 @@ public class AdminEndpoint {
     private AdminService adminService;
 
     @PostMapping("/admins")
-    Admin newParticipant(@RequestBody Admin newAdmin) {
+    public Admin newAdmin(@RequestBody Admin newAdmin) {
         return adminService.save(newAdmin);
     }
 
     @GetMapping(value="/admins")
-    public List<Admin> getAllParticipants(){
-        return (List<Admin>) adminService.findAll();
+    public Set<Admin> getAllAdmins(){
+        return (Set<Admin>) adminService.findAll();
     }
 
     @GetMapping(value = "admins/{id}", produces = "application/json")
-    public Admin getParticipant(@PathVariable long id) {
+    public Admin getAdmin(@PathVariable long id) {
         return adminService.findById(id)
                 .orElseThrow(() -> new AdminNotFoundException(id));
     }
 
     @PutMapping("/admins/{id}")
-    Admin replaceAdmin(@RequestBody Admin newAdmin, @PathVariable Long id) {
+    public Admin replaceAdmin(@RequestBody Admin newAdmin, @PathVariable Long id) {
         return adminService.findById(id)
-                .map(participant -> {
-                    participant.setEmail(newAdmin.getEmail());
-                    participant.setPassword(newAdmin.getPassword());
-                    return adminService.save(participant);
+                .map(admin -> {
+                    admin.setEmail(newAdmin.getEmail());
+                    admin.setPassword(newAdmin.getPassword());
+                    admin.setTournament(newAdmin.getTournament());
+                    return adminService.save(admin);
                 })
                 .orElseGet(() -> {
                     newAdmin.setId(id);
@@ -46,7 +47,7 @@ public class AdminEndpoint {
     }
 
     @DeleteMapping("/admins/{id}")
-    void deleteAdmin(@PathVariable Long id) {
+    public void deleteAdmin(@PathVariable Long id) {
         adminService.deleteById(id);
     }
 
