011ad39ace3df8ae64cb2b50276f6f708438a802
Student <sjoerd0707@live.nl>
2019-10-23 10:31:55 +0200

    Added NotModifiedException + Advice

--- src/main/java/YERgen2/demo/Exceptions/AlreadyEnrolledException.java
+++
@@ -1,7 +0,0 @@
-package YERgen2.demo.Exceptions;
-
-public class AlreadyEnrolledException extends RuntimeException {
-    public AlreadyEnrolledException(long pid, long eid){
-        super("Participant " + pid + " already enrolled in enrolment " + eid);
-    }
-}
