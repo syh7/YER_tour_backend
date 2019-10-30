f8914e9734f6406223bf97da6272f3d785d742b4
Wiljan Vinke <wiljanvinke@gmail.com>
2019-10-30 16:14:54 +0100

    Removed unused import from BettorEndpoint

--- src/main/java/YERgen2/demo/api/BettorEndpoint.java
+++ src/main/java/YERgen2/demo/api/BettorEndpoint.java
@@ -3,7 +3,6 @@ package YERgen2.demo.api;
 import YERgen2.demo.DTO.BettorDTO;
 import YERgen2.demo.controller.AccountService;
 import YERgen2.demo.controller.BetService;
-import YERgen2.demo.model.Bettor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
